93af618341822dc351b020a885a37532a99fd13e
Aleks901 <aleks-j29@hotmail.com>
2025-10-08 13:52:34 +0200

    Added global file (#13)
    # Description
    - Added global folder for things that are relevant for both desktop and mobile.

--- the-tavern-mobile/app/(user)/index.tsx
+++ the-tavern-mobile/app/(user)/index.tsx
@@ -1,7 +1,6 @@
 import {View, Text} from 'react-native';
 
 export default function UserProfile() {
-
   /*
     Reformatting this later to re-route to the logged in user's id 
     Just have to figure out how the heck we do it first :^)
