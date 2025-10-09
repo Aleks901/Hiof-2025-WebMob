85e826d821058bbaf6b3439322256b4f3b7f0c7a
Aleks901 <aleks-j29@hotmail.com>
2025-10-09 20:11:00 +0200

    ChatMessage interface (#14)
    # Description
    - Created an interface for ChatMessages.
    - Added some TsDoc work on types.

---
+++ global/types/chat-message.ts
@@ -0,0 +1,24 @@
+import { User } from "./user";
+/**
+ * Interface for chat messages.
+ * 
+ * @example
+ * ```typescript 
+ * const exampleMessage: ChatMessage = {
+ *  id: 3001,
+ *  message: "Hello!",
+ *  user: {
+ *      id: 123,
+ *      name: "Meowingtons",
+ *      role: "regular",
+ *      dateJoined: new Date("2025-11-15T09:00:00Z")
+ *  }
+ * };
+ * 
+ * ```
+ */
+export interface ChatMessage{
+    id: number,
+    message: string,
+    user: User
+}
--- global/types/role.ts
+++ global/types/role.ts
@@ -1,1 +1,9 @@
+/**
+ * Roles for the users
+ * ```
+ * 'regular' - A regular user
+ * 'admin' - An administrator user
+ * 'moderator' - A moderator user
+ * ```
+ */
 export type Role = 'regular' | 'admin' | 'moderator';
