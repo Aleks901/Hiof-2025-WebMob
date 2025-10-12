c2bbdf5c0f5016fb699fd811542b6fc15d2ab283
SimenKosberg <simen.kosberg@gmail.com>
2025-10-12 17:59:29 +0200

    added a component with a list of messages and added it to the mobile build (#27)

---
+++ the-tavern-mobile/components/tavern-chat/chat-list.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { FlatList, View } from "react-native";
+import { ChatMessage } from "../../../global/types/chat-message";
+import ChatMessageCard from "./chat-message";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+interface ChatListProps {
+  messages: ChatMessage[];
+}
+
+export default function ChatList({ messages }: ChatListProps) {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+        paddingHorizontal: insets.left,
+      }}
+    >
+      <FlatList
+        data={messages}
+        style={{ marginVertical: 20 }}
+        contentContainerStyle={{ gap: 15 }}
+        renderItem={({ item }) => <ChatMessageCard message={item} />}
+        keyExtractor={(item) => item.id.toString()}
+        ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
+      />
+    </View>
+  );
+}
