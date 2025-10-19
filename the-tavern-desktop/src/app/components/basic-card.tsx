416018f20332eb9172d85d3de49aa3bfce3ce021
Lukas <147519037+Lukazsn@users.noreply.github.com>
2025-10-19 19:51:27 +0200

    Created basic-card component
    Created a basic card component that takes an image?, title, description?, children?

---
+++ the-tavern-desktop/src/app/components/basic-card.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+
+
+type BasicCardProps = {
+    image?: string;
+    title: string;
+    description?: string;
+    children?: React.ReactNode;
+};
+
+export default function BasicCard({
+    image,
+    title,
+    description,
+    children,
+}: BasicCardProps) {
+    return (
+        <div style={styles.card}>
+            {image && <img src={image} alt={title} style={styles.image}/>}
+            <h3 style={styles.title}>{title}</h3>
+            {description && <p style={styles.description}>{description}</p>}
+            {children && <div style={styles.children}> {children}</div>}
+        </div>
+    )
+}
+
+const styles = {
+    card: {
+
+    },
+
+    image: {
+
+    }, 
+
+    title: {                            //Current skeleton placeholder for future styling
+
+    },
+
+    description: {
+
+    },
+
+    children: {
+
+    }
+
+}
