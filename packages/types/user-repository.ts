d7c28f2e1b3e7f936e73f2b89a03a050cf884c2c
Aleks901 <aleks-j29@hotmail.com>
2025-10-20 23:56:40 +0200

    Type safety
    Just making sure that we're getting a User object and nothing but a User object whenever these calls are made.

--- packages/types/user-repository.ts
+++ packages/types/user-repository.ts
@@ -1,9 +1,9 @@
 import { Result } from "./result";
-
+import { User } from "./user";
 
 export interface UserRepository {
-    findMany(params?: any): Promise<Result<any[]>>;
-    findById(id: number): Promise<Result<any | null>>;
-    create(userData: Partial<any>): Promise<Result<any>>;
-    update(id: number, userData: Partial<any>): Promise<Result<any | null>>;
+    findMany(params?: any): Promise<Result<User[]>>;
+    findById(id: number): Promise<Result<User | null>>;
+    create(userData: Partial<User>): Promise<Result<User>>;
+    update(id: number, userData: Partial<User>): Promise<Result<User | null>>;
 }
