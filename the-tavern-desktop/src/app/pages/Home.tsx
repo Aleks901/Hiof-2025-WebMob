2c581e3965a7cbe0dd4d8b604f95241bf8802cca
Aleks901 <aleks-j29@hotmail.com>
2025-10-21 11:44:27 +0200

    Fix for (#62)
    Added pages needed

--- the-tavern-desktop/src/app/pages/Home.tsx
+++ the-tavern-desktop/src/app/pages/Home.tsx
@@ -4,9 +4,7 @@ export function Home({ ctx }: RequestInfo) {
   return (
     <div>
       <p>
-        {ctx.user?.username
-          ? `You are logged in as user ${ctx.user.username}`
-          : "You are not logged in"}
+       
       </p>
     </div>
   );
---
+++ the-tavern-desktop/src/app/pages/UserPage.tsx
@@ -0,0 +1,10 @@
+
+
+export function UserPage() {
+  return (
+    <div>
+      <h1>User Page</h1>
+      <p>This is where user details will be displayed.</p>
+    </div>
+  );
+}
