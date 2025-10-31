4f779acd8c5540d7e2c154294a89dea18ee844ab
Stuebord <bastianwestbyhansen@gmail.com>
2025-10-31 16:01:54 +0100

    Login form and Auth context (#15) (#29)
    # Description
    Added auth context to handle login and register attempts (Still a work in progress)
    Used BasicForm to create loginform (Think its overkill to create a custom login form)
    Can close these issues, next step is using database to register and login

--- the-tavern-mobile/app/index.tsx
+++ the-tavern-mobile/app/index.tsx
@@ -1,10 +1,46 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import { Link } from 'expo-router';
+import { BasicForm } from '@/components/basic-form';
+import React from 'react';
+import { useUser }  from '@packages/hooks/useUser';
 
 export default function LoginScreen() {
+    const [username, setUsername] = React.useState('')
+    const [password, setPassword] = React.useState('')
+
+    const { user } = useUser();
+
+    const handleSubmit = () => {
+        console.log('Current user:',user)
+        console.log('Logging in with', username, password)
+    }
+
     return (
         <View>
-            <Text> Login screen </Text>
+            <BasicForm title="Login" onSubmit={handleSubmit}>
+                <TextInput
+                    placeholder="Username"
+                    value={username}
+                    onChangeText={setUsername}
+                    style={{ 
+                        margin: 6,
+                        borderWidth: 1,
+                        padding: 8,
+                        borderRadius: 6
+                     }}
+                    />
+                <TextInput
+                    placeholder="Password"
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry
+                    style={{ 
+                        margin: 6,
+                        borderWidth: 1,
+                        padding: 8,
+                        borderRadius: 6
+                     }}/>
+                </BasicForm>
             <Link href="/(drawer)/(home)"> Home </Link>
         </View>
     );
