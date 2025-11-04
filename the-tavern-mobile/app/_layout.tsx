445ef0714dbf1315e1d4a3cc18707a091af4879e
Lukas <147519037+Lukazsn@users.noreply.github.com>
2025-11-04 13:12:30 +0100

    Update for layout.tsx
    Update for layout.tsx, adding logo for 2 pages

--- the-tavern-mobile/app/_layout.tsx
+++ the-tavern-mobile/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Tabs } from "expo-router";
 import { FontAwesome } from "@expo/vector-icons";
 import { useTheme } from "@packages/ui/useTheme";
+import { Image } from "react-native";
 
 export default function RootLayout() {
   const theme = useTheme();
@@ -18,6 +19,18 @@ export default function RootLayout() {
         },
         tabBarActiveTintColor: theme.highlight,
         tabBarInactiveTintColor: theme.mutedText,
+
+        headerTitle: () => (
+          <Image source={require("../tmp/images/Logo.png")}
+          style={{
+            width: 120,
+            height: 120,
+            resizeMode: "contain",
+            alignSelf: "center",
+          }}
+          />
+        ),
+        headerTitleAlign: "center"
       }}
 
     >
