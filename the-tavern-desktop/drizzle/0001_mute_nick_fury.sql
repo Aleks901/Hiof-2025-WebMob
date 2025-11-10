6bd6510b236f7355a20da4ae5a2e9f7dba964839
SimenKosberg <simen.kosberg@gmail.com>
2025-11-10 22:57:51 +0100

    fix: reorder migration to resolve FK constraint error

--- the-tavern-desktop/drizzle/0001_mute_nick_fury.sql
+++ the-tavern-desktop/drizzle/0001_mute_nick_fury.sql
@@ -1,11 +1,3 @@
-CREATE TABLE `user_friends` (
-	`user_id` integer NOT NULL,
-	`friend_id` integer NOT NULL,
-	PRIMARY KEY(`user_id`, `friend_id`),
-	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action,
-	FOREIGN KEY (`friend_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
-);
---> statement-breakpoint
 PRAGMA foreign_keys=OFF;--> statement-breakpoint
 CREATE TABLE `__new_users` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -19,4 +11,11 @@ CREATE TABLE `__new_users` (
 INSERT INTO `__new_users`("id", "name", "password", "joined_at", "role", "token") SELECT "id", "name", "password", "joined_at", "role", "token" FROM `users`;--> statement-breakpoint
 DROP TABLE `users`;--> statement-breakpoint
 ALTER TABLE `__new_users` RENAME TO `users`;--> statement-breakpoint
-PRAGMA foreign_keys=ON;
+PRAGMA foreign_keys=ON;--> statement-breakpoint
+CREATE TABLE `user_friends` (
+	`user_id` integer NOT NULL,
+	`friend_id` integer NOT NULL,
+	PRIMARY KEY(`user_id`, `friend_id`),
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`friend_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
